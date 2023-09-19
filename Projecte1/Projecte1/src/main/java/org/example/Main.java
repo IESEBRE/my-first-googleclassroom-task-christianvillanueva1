daf90f52293da471736c7302d9db0f528bbfef97
Christian Villanueva <christianvillanueva@iesebre.com>
2023-09-19 13:14:54 +0200

    He afegit el primer projecte Java del curs

---
+++ Projecte1/Projecte1/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //sout para atajo rapit
+        System.out.println("Hello world!");
+        System.out.print("Me dic Christian ");
+        System.out.println("Villanueva Lor");
+    }
+}
