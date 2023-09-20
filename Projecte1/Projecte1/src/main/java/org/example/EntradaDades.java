2761d78961456fc2a8aea2a64247ccf7b1e80dd7
Christian Villanueva <christianvillanueva@iesebre.com>
2023-09-20 12:24:36 +0200

    20set

---
+++ Projecte1/Projecte1/src/main/java/org/example/EntradaDades.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class EntradaDades {
+    public static void main(String[] args) {
+
+        int valor;
+        Scanner ent = new Scanner(System.in);
+
+        valor = ent.nextInt();
+        System.out.println(valor);
+    }
+}
--- Projecte1/Projecte1/src/main/java/org/example/Main.java
+++ Projecte1/Projecte1/src/main/java/org/example/Main.java
@@ -6,5 +6,12 @@ public class Main {
         System.out.println("Hello world!");
         System.out.print("Me dic Christian ");
         System.out.println("Villanueva Lor");
+
+        //Declaracions de variables
+        float velocitat = 1.234567890F;
+        char lletra = 'G';
+        boolean pregunta = true;
+        int num = 89;
+        String text = "Algo que necesitem";
     }
 }
